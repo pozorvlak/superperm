fdfdbf1c6459c618d7846b36ac00f57c6a9c16b9
Miles Gould <miles@assyrian.org.uk>
2019-05-22 13:36:40 +0100

    Rewind algo from Stevens & Williams
    I still have no idea how it's meant to be used :-(

--- bin/recycle_sequence.py
+++ bin/recycle_sequence.py
@@ -2,8 +2,7 @@ import sys
 
 
 def recycle_sequence(n):
-    a = [0 for i in range(n)]
-    f = list(range(1, n+1))
+    global a, f
     while f[0] < n:
         j = f[0]
         f[0] = 1
@@ -17,14 +16,14 @@ def recycle_sequence(n):
 
 
 def reuse(n, r):
+    global a, f
     for i in range(1, n):
         yield(n)
     yield(n - r + 1)
 
 
 def recycle_sequence_reuse(n):
-    a = [0 for i in range(n)]
-    f = list(range(1, n+1))
+    global a, f
     while f[0] < n:
         j = f[0]
         f[0] = 1
@@ -38,5 +37,26 @@ def recycle_sequence_reuse(n):
         yield v
     f[0] = 1
 
+
+def rewind(n, m, seq):
+    for v in seq(n-1):
+        yield v
+    yield n-1
+    yield n
+    for i in range(1, m-1):
+        for v in seq(n-1):
+            yield n
+    for v in seq(n-1):
+        yield v
+    yield n-1
+    yield n
+
+
 n = int(sys.argv[1])
-print(list(recycle_sequence_reuse(n)))
+a = [0 for i in range(n)]
+f = list(range(1, n+1))
+for i in range(n):
+    rsr = list(rewind(n, i, recycle_sequence_reuse))
+    print(rsr)
+    print(len(rsr))
+
